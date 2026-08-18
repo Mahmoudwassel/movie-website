7eb732f3a884ebf972d84c31bf07253b748e3cf4
wassel <mahmoudanwarwassel981@gmail.com>
2026-08-18 17:01:28 +0300

    last fixed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import store from './app/store'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}> 
       <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/movie-website">
             <App />
         </BrowserRouter>
     </StrictMode>
